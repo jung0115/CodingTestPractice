eea914e4585218086f91797417ee15baed054df2
jung0115 <jungim7490@gmail.com>
2022-01-14 17:51:26 +0900

    백준 2문제

--- Java/Beakjoon/단계별로_풀어보기/브루트_포스/Num1436.java
+++ Java/Beakjoon/단계별로_풀어보기/브루트_포스/Num1436.java
@@ -1,7 +1,6 @@
 //백준 > 단계별로 풀어보기 > 브루트 포스 > 영화감독 숌(1436)
 package Java.Beakjoon.단계별로_풀어보기.브루트_포스;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 public class Num1436 {
     public static void main(String[] args) {
@@ -10,22 +9,21 @@ public class Num1436 {
         N = scan.nextInt();
         scan.close();
 
-        int cnt = 1, endNum = 666;
-        while(cnt != N) {
-            endNum = endNumber(endNum);
-            cnt++;
+        int num = 666, cnt = 1;
+        while(N != cnt) {
+            num++;
+            if(checkEndNumber(num)) cnt++;
         }
-        System.out.println(endNum);
+        System.out.println(num);
     }
-    static int endNumber(int num){
-        ArrayList<Integer> numArray = new ArrayList<>();
-        int check = num;
-        while(check > 0) {
-            numArray.add(check%10);
-            check /= 10;
+    static boolean checkEndNumber(int num) {
+        int cnt = 0;
+        while(num > 0){
+            if(num%10 == 6) cnt++;
+            else cnt = 0;
+            num /= 10;
+            if(cnt == 3) return true;
         }
-        
-        if()
-        //나중에 이어서 풀기
+        return false;
     }
 }
---
+++ Java/Beakjoon/단계별로_풀어보기/정렬/Num2750.java
@@ -0,0 +1,30 @@
+//백준 > 단계별로 풀어보기 > 브루트 포스 > 수 정렬하기(2750)
+package Java.Beakjoon.단계별로_풀어보기.정렬;
+
+import java.util.Scanner;
+public class Num2750 {
+    public static void main(String[] args) {
+        int N;
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        int[] num = new int[N];
+        for(int i = 0; i < N; i++) {
+            num[i] = scan.nextInt();
+        }
+        scan.close();
+
+        for(int i = 0; i < N-1; i++) {
+            int min = i;
+            for(int j = i+1; j < N; j++)
+                if(num[j] < num[min]) min = j;
+            
+            if(min != i){
+                int tmp = num[min];
+                num[min] = num[i];
+                num[i] = tmp;
+            }
+            System.out.println(num[i]);
+        }
+        System.out.println(num[N-1]);
+    }
+}
