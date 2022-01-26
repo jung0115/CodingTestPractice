18f89c99fc9f2fbf8a5e145de2ccca0dc395a317
jung0115 <jungim7490@gmail.com>
2022-01-26 15:04:05 +0900

    백트래킹 4문제

--- Java/Beakjoon/단계별로_풀어보기/백트래킹/Num15652.java
+++ Java/Beakjoon/단계별로_풀어보기/백트래킹/Num15652.java
@@ -1,17 +1,17 @@
-//백준 > 단계별로 풀어보기 > 백트래킹 > N과 M 1(15649)
+//백준 > 단계별로 풀어보기 > 백트래킹 > N과 M 4(15652)
 package Java.Beakjoon.단계별로_풀어보기.백트래킹;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-public class Num15649 {
-    public static void main(String[] args) throws IOException {
+public class Num15652 {
+    public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
+
         int[] num = new int[M];
         StringBuilder printSet = new StringBuilder();
         printSet = NandM(N, M, 0, num);
@@ -20,8 +20,8 @@ public class Num15649 {
     static StringBuilder NandM(int N, int M, int cnt, int[] num) {
         StringBuilder sequence = new StringBuilder();
         
-        for(int i = 0; i < cnt-1; i++) //수열 중 겹치는 수가 있을 경우
-            if(num[cnt-1] == num[i]) return sequence;
+        for(int i = 0; i < cnt-1; i++) //수열이 비내림차순이 아닌 경우
+            if(num[cnt-1] < num[i]) return sequence;
         
         if(cnt == M) { //수열 완성
             for(int i = 0; i < M; i++)
